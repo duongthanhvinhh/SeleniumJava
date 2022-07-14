c261d1c47e06baae397b9f99c0e578fb7c8353fa
vinhtester <vinhdtvt1999@gmail.com>
2022-07-15 00:49:31 +0700

    update project

--- src/test/java/vinhtester/com/BasePM/BaseTest.java
+++ src/test/java/vinhtester/com/BasePM/BaseTest.java
@@ -6,28 +6,36 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.time.Duration;
+
 public class BaseTest {
 
     public static WebDriver driver;
 
-    public void createDriver(String driverType) {
+    public static void createDriver(String driverType) {
         switch (driverType) {
             case "Chrome": {
                 WebDriverManager.chromedriver().setup();
                 driver = new ChromeDriver();
                 driver.manage().window().maximize();
+                driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+                driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
                 break;
             }
             case "FireFox": {
                 WebDriverManager.firefoxdriver().setup();
                 driver = new FirefoxDriver();
                 driver.manage().window().maximize();
+                driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+                driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
                 break;
             }
             case "Edge": {
                 WebDriverManager.edgedriver().setup();
                 driver = new EdgeDriver();
                 driver.manage().window().maximize();
+                driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+                driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
                 break;
             }
             default: {
@@ -35,11 +43,15 @@ public class BaseTest {
                 WebDriverManager.chromedriver().setup();
                 driver = new ChromeDriver();
                 driver.manage().window().maximize();
+                driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+                driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
             }
         }
     }
 
-    public void closeDriver() {
-        driver.close();
+    public static void closeDriver() {
+        if (driver != null) {
+            driver.quit();
+        }
     }
 }
--- src/test/java/vinhtester/com/LearnWebElement/checkBox.java
+++ src/test/java/vinhtester/com/LearnWebElement/checkBox.java
@@ -1,2 +1,44 @@
-package vinhtester.com.LearnWebElement;public class checkBox {
+package vinhtester.com.LearnWebElement;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+import vinhtester.com.BasePM.BaseTest;
+
+public class checkBox extends BaseTest {
+
+    @Test
+    public void singleCheckbox() {
+        createDriver("Chrome");
+        driver.get("https://demo.anhtester.com/basic-checkbox-demo.html");
+        boolean isAgeSelected = driver.findElement(By.xpath("//input[@id='isAgeSelected']")).isSelected();
+        if (!isAgeSelected) {
+            driver.findElement(By.xpath("//input[@id='isAgeSelected']")).click();
+        }
+        isAgeSelected = driver.findElement(By.xpath("//input[@id='isAgeSelected']")).isSelected();
+        if(isAgeSelected){
+            System.out.println("Check single box: PASSED");
+        }else{
+            System.out.println("Check single box: FAILED");
+        }
+        closeDriver();
+    }
+    @Test
+    public void multipleCheckbox() {
+        createDriver("Chrome");
+        driver.get("https://demo.anhtester.com/basic-checkbox-demo.html");
+        //boolean isAgeSelected = driver.findElement(By.xpath("(//input[@type='checkbox'])[2]")).isSelected();
+        boolean isAgeSelected = driver.findElement(By.xpath("//label[normalize-space()='Option 1']/child::input")).isSelected();
+        if (!isAgeSelected) {
+            //driver.findElement(By.xpath("(//input[@type='checkbox'])[2]")).click();
+            driver.findElement(By.xpath("//label[normalize-space()='Option 1']/child::input")).click();
+        }
+        //isAgeSelected = driver.findElement(By.xpath("(//input[@type='checkbox'])[2]")).isSelected();
+        isAgeSelected = driver.findElement(By.xpath("//label[normalize-space()='Option 1']/child::input")).isSelected();
+        if(isAgeSelected){
+            System.out.println("Check multiple box: PASSED");
+        }else{
+            System.out.println("Check multiple box: FAILED");
+        }
+        closeDriver();
+    }
 }
