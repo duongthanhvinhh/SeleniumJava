43125125ca72b24213b2583a114b606c54b2742e
vinhtester <vinhdtvt1999@gmail.com>
2022-07-15 10:31:53 +0700

    fix issue firstTest.java

--- src/test/java/vinhtester/com/firstExercise/firstTest.java
+++ src/test/java/vinhtester/com/firstExercise/firstTest.java
@@ -21,19 +21,22 @@ public class firstTest {
         driver.findElement(By.id("iusername")).sendKeys("admin01");
         driver.findElement(By.id("ipassword")).sendKeys("123456");
         driver.findElement(By.xpath("//button[@type='submit']")).click();
+        Thread.sleep(5000);
 
 
         //Get title's page:
-        Thread.sleep(3000);
-        String a = driver.getTitle();
-        System.out.println("Title of the Page is: "+ a );
-        Thread.sleep(8000);
-        WebElement x = driver.findElement(By.xpath("//span[contains(text(),'Dự án')]"));
-        x.click();
-        driver.findElement(By.xpath("//div[@class='card-header-right']//a[contains(.,'Thêm mới')]")).click();
-        driver.findElement(By.xpath("//div[@class='form-group']/descendant::input[@placeholder='Tiêu đề']")).sendKeys("Duong Thanh Vinh");
-        driver.findElement(By.xpath("//span[@id='select2-client_id-container']")).click();
-        driver.findElement(By.xpath("//li[contains(text(),'Huong Nguyen')]")).click();
+//        Thread.sleep(3000);
+//        String a = driver.getTitle();
+//        System.out.println("Title of the Page is: "+ a );
+//        Thread.sleep(8000);
+//        driver.findElement(By.xpath("//body/header[@class='pc-header bg-dark']/div[@class='header-wrapper']/div[@class='ml-auto']/ul[@class='list-unstyled']/li[1]/a[1]/img[1]")).click();
+//        driver.findElement(By.xpath("//span[normalize-space()='English']"));
+//        WebElement x = driver.findElement(By.xpath("//span[contains(text(),'Dự án')]"));
+//        x.click();
+//        driver.findElement(By.xpath("//div[@class='card-header-right']//a[contains(.,'Thêm mới')]")).click();
+//        driver.findElement(By.xpath("//div[@class='form-group']/descendant::input[@placeholder='Tiêu đề']")).sendKeys("Duong Thanh Vinh");
+//        driver.findElement(By.xpath("//span[@id='select2-client_id-container']")).click();
+//        driver.findElement(By.xpath("//li[contains(text(),'Huong Nguyen')]")).click();
 
         //Close browser:
         driver.quit();
